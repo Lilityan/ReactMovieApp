92c2c774bdbc147b4820c59a0e3fd428b637ffcf
Lilit Petrosyan <lilit_petrosyan@epam.com>
2022-06-27 15:27:40 +0400

    Added search functionality

--- react-movie-app/src/pages/Favorite.js
+++ react-movie-app/src/pages/Favorite.js
@@ -1,62 +1,21 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import MovieCard from '../components/MovieCard';
-import axios from 'axios';
 import 'react-lazy-load-image-component/src/effects/blur.css';
-import useInfiniteScroll from 'react-infinite-scroll-hook';
 import { useSelector } from 'react-redux';
 
 const Favorite = () => {
-
-	const [data, setData] = useState([]);
-	const [currentPage, setCurrentPage] = useState(1);
-	const [loading, setLoading] = useState(false);
-	const [hasNextPage, setHasNextPage] = useState(true);
-
-	const { favoriteMoivies: { results: movies } } = useSelector(state => state.favoriteMoivies);
-	const moviesPerPage = 20;
-
-	const getmore = async () => {
-
-		try {
-			const nextMovies = movies.slice[(currentPage-1)*moviesPerPage, currentPage*moviesPerPage]
-			return setData((data) => [...data, ...nextMovies]);
-		}
-		catch (e) {
-			console.log(e);
-			return setHasNextPage(false);
-		}
-		finally {
-			setCurrentPage(currentPage + 1)
-			return setLoading(false);
-		}
-
-	}
-
-	const [sentryRef] = useInfiniteScroll({
-		loading,
-		hasNextPage: hasNextPage,
-		delayInMs: 50,
-		onLoadMore: () => {
-			getmore(currentPage);
-		}
-	})
-
+	const favoriteMovies = useSelector(state => state.favoriteMovies);
 
 	return (
 		<>
-			<h1>Favorite Movies</h1>
-			<Container fluid>
-				<Row lg={'auto'}>
-					{data.map((movie, index) => { return (<Col sm key={index}><MovieCard movie={movie} /></Col>) })}
-				</Row>
-			</Container>
-			{
-				(loading || hasNextPage) &&
-				<div className="loader" ref={sentryRef}>
-					<h1>Loading...</h1>
-				</div>
-			}
+			{favoriteMovies ? <><h1>Favorite Movies</h1>
+				<Container fluid>
+					<Row lg={'auto'}>
+						{favoriteMovies.favoriteMovies.map((movie, index) => { return (<Col sm key={index}><MovieCard movie={movie} /></Col>) })}
+					</Row>
+				</Container>
+			</> : <>There is no added favorite movies</>}
 
 		</>
 	);
--- react-movie-app/src/pages/Search.js
+++ react-movie-app/src/pages/Search.js
@@ -1,13 +1,42 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { Container, Row, Col } from 'react-bootstrap';
+import MovieCard from '../components/MovieCard';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 
 const Search = () => {
-return (
-	<div>
-	<h1>
-	Search
-	</h1>
-	</div>
-);
+
+	const [searchKey, setSearchKey] = useState("");
+	const [movies, setMovies] = useState([]);
+	const getData = async () => {
+		const data = axios.get(`/search/movie`, {
+			params: {
+				api_key: process.env.REACT_APP_MOVIE_API_KEY,
+				query: searchKey
+			}
+		}).then(res => setMovies([...res.data.results]))
+	}
+
+	const handleSearchClick = () => {
+		getData();
+	}
+
+
+	return (
+		<>
+			<input
+				style={{ width: "400px", margin: "10px" }}
+				onChange={(e) => setSearchKey(e.target.value)}
+			/>
+			<button type="submit" onClick={handleSearchClick}>Search</button>
+			{movies ? <><h1>{`Results by searching "${searchKey}"`}</h1>
+				<Container fluid>
+					<Row lg={'auto'}>
+						{movies.map((movie, index) => { return (<Col sm key={index}><MovieCard movie={movie} /></Col>) })}
+					</Row>
+				</Container>
+			</> : <>There is no any movies by your search</>}
+		</>);
 };
 
 export default Search;
